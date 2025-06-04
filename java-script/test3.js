b442b369d11d6c295d8c831bd92680118d9fd41a
AmanSuthar1312 <164989741+AmanSuthar1312@users.noreply.github.com>
2025-06-04 10:46:17 +0530

    creating new page

--- java-script/test3.js
+++ java-script/test3.js
@@ -8,27 +8,26 @@ let attempts = 0;
 let inp;
 let playing = true;
 
-// while (playing) {
-//     inp = window.prompt("Enter a number between 1 and 10");
-//     inp = Number(inp);
+while (playing) {
+    inp = window.prompt("Enter a number between 1 and 10");
+    inp = Number(inp);
 
-//     if (isNaN(inp)) {
-//         window.alert("Enter a valid number");
-//     }
-//     else if (inp < min || inp > max) {
-//         window.alert("Enter a number between 1 and 10");
-//     }
-//     else {
-//         attempts++;
-//         if (inp > Answer) {
-//             window.alert("The number is too big. Try again!");
-//         }
-//         else if (inp < Answer) {
-//             window.alert("The number is too small. Try again!");
-//         }
-//         else {
-//             window.alert(`You guessed it right! The number was ${Answer}. It took you ${attempts} attempts.`);
-//             playing = false;
-//         }
-//     }
-// }
+    if (isNaN(inp)) {
+        window.alert("Enter a valid number");
+    }
+    else if (inp < min || inp > max) {
+        window.alert("Enter a number between 1 and 10")
+    }
+    else {
+        attempts++;
+        if (inp > Answer) {
+            window.alert("The number is too big. Try again!");
+        }
+        else if (inp < Answer) {
+            window.alert("The number is too small. Try again!");
+        }
+        else window.alert(`You guessed it right! The number was ${Answer}. It took you ${attempts} attempts.`);
+        playing = false;
+        }
+    }
+
